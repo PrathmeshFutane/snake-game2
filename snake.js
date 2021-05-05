e132b0c77b0977fb404a72b91ed611a9829d2623
PrathmeshFutane <63443691+PrathmeshFutane@users.noreply.github.com>
2021-05-05 15:42:07 +0530

    changing the speed of snake

--- snake.js
+++ snake.js
@@ -1,12 +1,12 @@
 import { getInputDirection } from "./input.js";
 
 let newSegments = 0;
-export const SNAKE_SPEED =19;
+export const SNAKE_SPEED =9;
 const snakeBody = [{x:11, y:11}]
 
 export function update(){
     addSegments()
-    console.log('update snake')
+    
     const inputDirection = getInputDirection()
     for(let i =  snakeBody.length -2; i>=0; i--){
         snakeBody[i+1]={...snakeBody[i]}
@@ -17,7 +17,7 @@ export function update(){
 }
 
 export function draw(gameBoard){
-    console.log('draw snake')
+    
     snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div');
         snakeElement.style.gridRowStart = segment.y;
